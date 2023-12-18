acbc1f167f1f7fddc958495bb9a6e2dee5c618c6
Lena <120247327+LenaPosh@users.noreply.github.com>
2023-12-19 00:44:28 +0400

    add activeClass

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -13,15 +13,42 @@ const NavMenu = () => {
                 </div>
 
                 <div className="nav-links">
-                    <NavLink to="/home" className="nav_item">Home</NavLink>
-                    <NavLink to="/about" className="nav_item">Our Products</NavLink>
-                    <NavLink to="/explore" className="nav_item">Explore</NavLink>
-                    <NavLink to="/prices" className="nav_item">Prices</NavLink>
+                    <NavLink
+                        to="/home"
+                        className={({ isActive }) => isActive ? "nav_item active" : "nav_item"}
+                    >
+                        Home
+                    </NavLink>
+                    <NavLink
+                        to="/about"
+                        className={({ isActive }) => isActive ? "nav_item active" : "nav_item"}
+                    >
+                        Our Products
+                    </NavLink>
+                    <NavLink
+                        to="/explore"
+                        className={({ isActive }) => isActive ? "nav_item active" : "nav_item"}
+                    >
+                        Explore
+                    </NavLink>
+                    <NavLink
+                        to="/prices"
+                        className={({ isActive }) => isActive ? "nav_item active" : "nav_item"}
+                    >
+                        Prices
+                    </NavLink>
+
                 </div>
 
                 <div className="nav-buttons">
                     <button className="button-menu">WhitePaper</button>
-                    <NavLink to="/signin" className="nav_item">Sign In</NavLink>
+                    <NavLink
+                        to="/signin"
+                        className={({ isActive }) => isActive ? "nav_item active" : "nav_item"}
+                    >
+                        Sign In
+                    </NavLink>
+
                 </div>
             </div>
         </div>
